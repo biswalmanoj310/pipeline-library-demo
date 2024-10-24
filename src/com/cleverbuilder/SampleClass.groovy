ca883a49a50c1d7719525436237eb857dc7bd758
manojbiswalcollege <manojbiswalcollege@gmail.com>
2024-10-23 19:13:37 -0700

    committing the changes.

---
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+package com.cleverbuilder
+
+class SampleClass {
+   String name
+   Integer age
+
+   def increaseAge(Integer years) {
+      this.age += years
+   }
+}
+
+
---
+++ vars/sayHello.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(String name = 'human') {
+  echo "Hello, ${name}."
+}
+
+
+
+
